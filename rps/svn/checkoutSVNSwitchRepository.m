4c4c61800bac2c136b21f71cc2237ed99f5bf0a0
dakmord <daniel.schneider.dsch@gmail.com>
2015-06-01 13:40:11 +0200

    Bugfix, replaced rmdir routine for switching repository,

--- rps/svn/checkoutSVNSwitchRepository.m
+++ rps/svn/checkoutSVNSwitchRepository.m
@@ -33,12 +33,32 @@ rmpath(fullfile(homeDir,'help'));
 rmpath(fullfile(homeDir,'help', 'html'));
 
 % Delete old repo folders..
-cmd = sprintf('rd /S /Q %s',fullfile(homeDir,'blocks'));
-system(cmd,'-echo');
-cmd = sprintf('rd /S /Q %s',fullfile(homeDir,'rps'));
-system(cmd,'-echo');
-cmd = sprintf('rd /S /Q %s',fullfile(homeDir,'help'));
-system(cmd,'-echo');
+% RPS
+delete(fullfile(homeDir, 'rps', '*.m'))
+delete(fullfile(homeDir, 'rps', '*.fig'))
+delete(fullfile(homeDir, 'rps', '*.xml'))
+delete(fullfile(homeDir, 'rps', '*.exe'))
+delete(fullfile(homeDir, 'rps', '*.txt'))
+rmdir(fullfile(homeDir, 'rps', '*.*'),'s')
+rmdir(fullfile(homeDir,'rps'),'s')
+% Help
+delete(fullfile(homeDir, 'help', '*.m'))
+delete(fullfile(homeDir, 'help', '*.fig'))
+delete(fullfile(homeDir, 'help', '*.xml'))
+delete(fullfile(homeDir, 'help', '*.exe'))
+delete(fullfile(homeDir, 'help', '*.txt'))
+rmdir(fullfile(homeDir, 'help', '*.*'),'s')
+rmdir(fullfile(homeDir,'help'),'s')
+% Blocks
+delete(fullfile(homeDir, 'blocks', '*.m'))
+delete(fullfile(homeDir, 'blocks', '*.slx'))
+delete(fullfile(homeDir, 'blocks', '*.mdl'))
+delete(fullfile(homeDir, 'blocks', '*.fig'))
+delete(fullfile(homeDir, 'blocks', '*.xml'))
+delete(fullfile(homeDir, 'blocks', '*.exe'))
+delete(fullfile(homeDir, 'blocks', '*.txt'))
+rmdir(fullfile(homeDir, 'blocks', '*.*'),'s')
+rmdir(fullfile(homeDir,'blocks'),'s')
 
 % Wait a sec...
 pause(1);
